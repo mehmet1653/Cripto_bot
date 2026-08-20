13ab46a1d10ec4829a961d289edc370e7b7f8718
mehmet1653 <mehmetaa674@gmail.com>
2026-08-20 20:25:55 +0300

    Update kripto_ajani.py

--- kripto_ajani.py
+++ kripto_ajani.py
@@ -20,7 +20,7 @@ def home():
 def run_web():
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
+    
 # ==========================================
 # ⚙️ AYARLAR VE HAFIZA (KOMİSYON DAHİL)
 # ==========================================
